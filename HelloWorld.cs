26596bc4539c47882f45e988462eb9137b990470
zaananasb <130475311+zaananasb@users.noreply.github.com>
2024-12-04 18:53:28 +0300

    Добавили скрипт
    Этот скрипт выводит в консоль фразу Привет, Мир!

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Добавил код для сообщения "Hello, World!"
+        print("Hello, World!");
+    }
+
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
